20e00bd328f18fd218afaace3fbc9e5db5883f22
been2 <nrjs3k@gmail.com>
2022-03-09 19:08:05 +0900

    화면구상

---
+++ front/src/components/menus/SentenceQuiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function SentenceQuiz() {
+  return (
+    <div>
+      <h2>문장 퀴즈</h2>
+    </div>
+  );
+}
+
+export default SentenceQuiz;
---
+++ front/src/components/menus/VoiceQuiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function VoiceQuiz() {
+  return (
+    <div>
+      <h2>음성 퀴즈</h2>
+    </div>
+  );
+}
+
+export default VoiceQuiz;
