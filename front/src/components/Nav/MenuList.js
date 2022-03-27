93ecf65edaf1cb3c5e522ec2ffdb36cc9c13d3f3
been2 <nrjs3k@gmail.com>
2022-03-28 02:13:25 +0900

    이미지 업로드 기능 추가

---
+++ front/src/components/Nav/MenuList.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useMenuDispatch, useMenuState } from '../../MenuContext';
+import { StyledLink } from '../Styles';
+
+const MenuListBlock = styled.div`
+  border-bottom: 1px solid #e9ecef;
+`;
+
+const ItemList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-align: center;
+
+  .focused {
+    background-color: lightgreen;
+  }
+`;
+
+const Item = styled.li`
+  padding: 10px 20px 10px 20px;
+  margin: 5px 10px 5px 10px;
+  margin-left: -1px;
+  display: inline-block;
+  border: 1px solid #e9ecef;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+  border-radius: 8px;
+`;
+
+function MenuList() {
+  const menus = useMenuState();
+  const dispatch = useMenuDispatch();
+  return (
+    <MenuListBlock>
+      <ItemList>
+        {menus.map((menu, index) => {
+          return (
+            <Item
+              key={index}
+              className={menu.focused === true ? 'focused' : ''}
+            >
+              <StyledLink
+                to={menu.link}
+                onClick={() => dispatch({ type: 'TOGGLE', index })}
+              >
+                {menu.name}
+              </StyledLink>
+            </Item>
+          );
+        })}
+      </ItemList>
+    </MenuListBlock>
+  );
+}
+
+export default MenuList;
--- front/src/components/menus/SentenceQuiz.js
+++ front/src/components/menus/SentenceQuiz.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import PhotoTransfer from './PhotoTransfer';
 
 function SentenceQuiz() {
   return (
     <div>
       <h2>문장 퀴즈</h2>
+      <PhotoTransfer />
     </div>
   );
 }
